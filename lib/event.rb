f06311ed01b668007942e6674709fd110d7ab0bf
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-12 15:59:22 -0600

    WIP total_inventory method.

--- lib/event.rb
+++ lib/event.rb
@@ -23,4 +23,19 @@ class Event
     end
   end
 
+  def total_inventory
+    event_inventory = Hash.new(0)
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, amount|
+        event_inventory[item] = {quantity: amount, food_trucks: food_trucks_that_sell(item)}
+        # if event_inventory[item].nil?
+        #   event_inventory[item] = {quantity: amount, food_trucks: food_trucks_that_sell(item)}
+        # elsif !event_inventory[item].nil?
+        #   event_inventory[item][:quantity] + amount
+        # end
+      end
+    end
+    event_inventory
+  end
+
 end
