b795d1dba32b3a7ef0df2134c469e1fcdab6d7b0
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-12 15:02:23 -0600

    Add method to display food truck names to Event class, all test pass.

--- lib/event.rb
+++ lib/event.rb
@@ -10,4 +10,10 @@ class Event
   def add_food_truck(food_truck_obj)
     @food_trucks.push(food_truck_obj)
   end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+  end
 end
