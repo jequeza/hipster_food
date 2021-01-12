b42a92e2bacf2d3d1d13782f4dd885d517232d30
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-12 14:21:35 -0600

    Add #check_stock to FoodTruck class, all test pass.

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -6,4 +6,12 @@ class FoodTruck
     @name      = name
     @inventory = {}
   end
+
+  def check_stock(item_obj)
+    if @inventory.include?(item_obj)
+      @inventory[item_obj]
+    else
+      0
+    end
+  end
 end
