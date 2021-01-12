63df51398f855c0f8d2577a528eafd807da34000
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-12 14:38:21 -0600

    Add tests for stocking items and displaying inventory.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -20,4 +20,33 @@ class FoodTruckTest < Minitest::Test
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     assert_equal 0, food_truck.check_stock(item1)
   end
+
+  def test_it_can_stock_items
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    assert_equal ({item1 => 30}), food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_add_more_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+  end
+
+  def test_it_can_stock_multiple_items
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+    assert_equal 12, food_truck.check_stock(item2)
+    expected = {item1 => 55, item2 => 12}
+    assert_equal expected, food_truck.inventory
+  end
 end
