7c8a00bdf626ccfe8c3ccee202e91db8e705cec2
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-12 14:20:59 -0600

    Add test for #check_stock of FoodTruck class.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -14,4 +14,10 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal ({}), food_truck.inventory
   end
+
+  def test_it_can_return_amount_of_stock_for_given_item
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    assert_equal 0, food_truck.check_stock(item1)
+  end
 end
